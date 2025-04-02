691ccb10f0c8d47ebf365a1b67d8e5f0daecc249
Julianaa <juliana.angarita.ch.2203@gmail.com>
2025-04-02 17:36:09 -0500

    primer commit

---
+++ src/components/AgregarServicio.jsx
@@ -0,0 +1,47 @@
+import LogoGymTrack from './../assets/imagenes/LogoGymTrack.png'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function AgregarServicio() {
+    return (
+        <>
+        <body>
+            <header>
+            <Container>
+                {/* Stack the columns on mobile by making one full-width and the other half-width */}
+                <Row>
+                    <Col xs={12} md={8}>
+                    xs=12 md=8
+                    <img src={LogoGymTrack} alt="img" />
+                    </Col>
+                    <Col xs={6} md={4}>
+                    xs=6 md=4
+                    </Col>
+                </Row>
+                    {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+                <Row>
+                    <Col xs={6} md={4}>
+                    xs=6 md=4
+                    </Col>
+                    <Col xs={6} md={4}>
+                    xs=6 md=4
+                    </Col>
+                    <Col xs={6} md={4}>
+                    xs=6 md=4
+                    </Col>
+                </Row>
+                    {/* Columns are always 50% wide, on mobile and desktop */}
+                <Row>
+                    <Col xs={6}>xs=6</Col>
+                    <Col xs={6}>xs=6</Col>
+                </Row>
+            </Container>
+            </header>
+        </body>
+        </>
+    );
+    
+}
+
+export default AgregarServicio;
