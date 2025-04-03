14246eb10de1c37e7112c8143ade7e782f3d5fe8
Julianaa <juliana.angarita.ch.2203@gmail.com>
2025-04-02 23:07:41 -0500

    segundo commit

--- src/components/AgregarServicio.jsx
+++ src/components/AgregarServicio.jsx
@@ -1,40 +1,38 @@
 import LogoGymTrack from './../assets/imagenes/LogoGymTrack.png'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from "react-bootstrap";
 
 function AgregarServicio() {
     return (
         <>
         <body>
             <header>
-            <Container>
+            <Container className="justify-content-center col-border">
                 {/* Stack the columns on mobile by making one full-width and the other half-width */}
                 <Row>
-                    <Col xs={12} md={8}>
+                    <Col xs={12} md={8} className="text-center col-border">
                     xs=12 md=8
                     <img src={LogoGymTrack} alt="img" />
                     </Col>
-                    <Col xs={6} md={4}>
-                    xs=6 md=4
+                    <Col xs={6} md={4} className="text-center col-border">
+                    xs=6 md=4 columna 1
                     </Col>
                 </Row>
                     {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
                 <Row>
-                    <Col xs={6} md={4}>
-                    xs=6 md=4
+                    <Col xs={6} md={4} className="text-center col-border">
+                    xs=6 md=4 columna 2
                     </Col>
-                    <Col xs={6} md={4}>
-                    xs=6 md=4
+                    <Col xs={6} md={4} className="text-center col-border">
+                    xs=6 md=4 columna 3
                     </Col>
-                    <Col xs={6} md={4}>
-                    xs=6 md=4
+                    <Col xs={6} md={4} className="text-center col-border">
+                    xs=6 md=4 columna 4
                     </Col>
                 </Row>
                     {/* Columns are always 50% wide, on mobile and desktop */}
                 <Row>
-                    <Col xs={6}>xs=6</Col>
-                    <Col xs={6}>xs=6</Col>
+                    <Col xs={6} className="text-center col-border">xs=6 columna 5</Col>
+                    <Col xs={6} className="text-center col-border">xs=6 columna 6</Col>
                 </Row>
             </Container>
             </header>
