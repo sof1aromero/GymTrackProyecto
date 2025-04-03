58d95ecd0cdf9c86984ffa7aeca3d2f27b9a96b4
Juanda <cruzjuanda8amail.com>
2025-04-03 16:56:55 -0500

    cambios 3 abril

--- src/components/RecuperarContrasena.jsx
+++ src/components/RecuperarContrasena.jsx
@@ -1,10 +1,41 @@
-
-import "../Estilos/RecuperarContrasena.css";
+import "../Estilos/Registro.css";
+import { Link } from "react-router-dom";
 
 function RecuperarContrasena() {
   return (
-    <div>
-        hola
+    <div className="contenedor">
+      <header className="titulo">
+        <img
+          src="src/imgs/Logo.jpeg"
+          alt="GYMTRACK Logo"
+          className="logo"/>
+      </header>
+
+      <section className="formulario">
+        <h2>Recuperar contraseña</h2>
+
+        <div className="campo">
+          <label htmlFor="correorecuperar">Ingresar el correo electrónico con el que se registro</label>
+          <input type="text" id="primerNombre" name="primerNombre" placeholder="Correo electrónico" required />
+        </div>
+
+        <button className="btnRegistrar">Confirmar</button>
+
+        <div className="campo">
+          <label htmlFor="codigorecuperar">Código enviado al correo</label>
+          <input type="text" id="codigorecuperar" name="codigorecuperar" placeholder="" />
+        </div>
+
+        <button className="btnRegistrar">Reestablecer contraseña</button>
+
+        <section className="opciones">
+          <p>
+            ¿Ya tienes cuenta? <Link to="/iniciar-sesion">Inicia sesión aquí</Link>
+          </p>
+        </section>
+      </section>
+
+      <footer className="footer">¿Quiénes somos?</footer>
     </div>
   );
 }
