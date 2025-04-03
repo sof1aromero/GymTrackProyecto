026aaa4b82329269dbe85b145d3f9503b73bae1c
santiago-mendez1016 <santiago.mendezpd@gmail.com>
2025-04-03 12:30:04 -0500

    segundo commit

---
+++ src/Components/Comp1_1.jsx
@@ -0,0 +1,13 @@
+function Comp1_1(){
+    return(
+    <>
+        <head>
+            <div>
+                <h1>GYMTRACK</h1>
+            </div>
+        </head>
+    </>
+    );
+}
+
+export default Comp1_1;
