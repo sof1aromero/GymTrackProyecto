93255038eeb903f8af82364714a6b434b8e8da66
Anamilena2005 <millerosa09@gmail.com>
2025-04-08 19:48:22 -0500

    subiendo cambios del componente factura

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import Registro from "./components/Registro";
-import RecuperarContrasena from "./components/RecuperarContrasena";
-import IniciarSesion from "./components/IniciarSesion";
+import React from 'react';
+import FacturaDescargable from './components/facturadescargable';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<IniciarSesion />} />
-      <Route path="/registro" element={<Registro />} />
-      <Route path="/recuperar-contrasena" element={<RecuperarContrasena />} />
-      <Route path="/iniciar-sesion" element={<IniciarSesion />} />
-    </Routes>
+    <div>
+      <FacturaDescargable />
+    </div>
   );
 }
 
