51eb016a593be089c5c067161aafaf182c4d304b
sebas2906perre <sebasleon20061988@gmail.com>
2025-04-08 23:42:14 -0500

    segundo commit

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CompOne from './src/Components/CompOne'
+
+function App() {
+
+
+  return (
+    <>
+    
+    <CompOne/>
+
+    
+    </>
+  )
+}
+
+export default App
