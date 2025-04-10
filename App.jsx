e6a3a81ef9d5b75ddb8c4a2500f915aaff387f18
sebas2906perre <sebasleon20061988@gmail.com>
2025-04-10 15:08:38 -0500

    cambios

--- App.jsx
+++ App.jsx
@@ -1,20 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import DivOne from './src/Components/DivOne'
-import NavBar from './src/Components/NavBar'
-import Footer from './src/Components/Footer'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import DivOne from "./src/Components/DivOne";
+import NavBar from "./src/Components/navBar";
+import Footer from "./src/Components/Footer";
 
 function App() {
-
-
   return (
     <>
-    <NavBar/>
-    <DivOne/>
-    <Footer/>   
-    
+      <NavBar />
+      <div>
+        <DivOne />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,21 +1,20 @@
-import "./estilos/NavBar.css"
+import "../estilos/NavBar.css";
 
-export default function NavBar() {
-    return (
-        <nav className="navsection1">
-            <div className="nav-left"> 
-                <h1>GYMTRACK</h1>
-                <ul className="nav-links">
-                    <li>ENTRENADORES</li>
-                    <li>SERVICIOS</li>
-                </ul>
-            </div>
+function NavBar() {
+        return (
+            <nav className="navsection1">
+                <div className="nav-left"> 
+                    <h1>GYMTRACK</h1>
+                    <ul className="nav-links">
+                        <li>ENTRENADORES</li>
+                        <li>SERVICIOS</li>
+                    </ul>
+                </div>
 
-            <div className="nav-right">
-            <button className="btn"><a href="/initSesion">Iniciar Sesion</a></button>
-            <button className="btn">Crear cuenta</button>
-            </div>
-        </nav>
-    )
-}
-
+                <div className="nav-right">
+                </div>
+            </nav>
+        )
+    }
+export default  NavBar;  
+    
